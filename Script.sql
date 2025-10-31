c31d39f3ba3a1c9091ea23b366f2aa2387caf036
D_O_O_M <virezka@gmail.com>
2025-10-31 20:45:43 +0300

    Alter Table leson

--- Script.sql
+++ Script.sql
@@ -37,8 +37,30 @@ FOREIGN KEY (user_id) REFERENCES users(id)
 ON UPDATE CASCADE 
 ON DELETE RESTRICT;
 
+USE telegram;
+ALTER TABLE users ADD COLUMN birthday datetime;
+ALTER TABLE users MODIFY COLUMN birthday date;
+#ALTER TABLE users RENAME COLUMN birthday TO date_of_birth;
+#ALTER TABLE users DROP COLUMN date_of_birth;
 
-
-
+#1 x M
+#DROP TABLE IF EXISTS  private_messages;
+CREATE TABLE privat_messages(
+	id SERIAL,
+	sender_id bigint UNSIGNED NOT NULL,
+	receiver_id bigint UNSIGNED NOT NULL,
+	media_type enum('text', 'image', 'audio', 'video'),
+	body text,
+	filename varchar(200),
+	created_at datetime DEFAULT NOW(),
+	
+	FOREIGN KEY (sender_id) REFERENCES users(id),
+	FOREIGN KEY (receiver_id) REFERENCES users(id)
+	); 
+	
+	ALTER TABLE user_settings MODIFY COLUMN  user_id bigint UNSIGNED NOT NULL PRIMARY KEY;  
+	
+	ALTER TABLE user_settings 
+	RENAME  COLUMN  LANGUAGE TO app_language;
 
 
